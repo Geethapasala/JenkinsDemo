81818aec0d9ac84b16bba3d1a0852f762d354db6
Geethanjali_Pasala <Geethanjali_Pasala@epam.com>
2022-05-18 15:18:13 +0530

    jenkinsdemo

---
+++ src/test/java/com/jenkins/TestJenkins.java
@@ -0,0 +1,18 @@
+package com.jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestJenkins
+{
+	@Test
+	public void Check()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Geethanjali_Pasala\\Downloads\\chromedriver_win32/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		driver.quit();
+	}
+}
